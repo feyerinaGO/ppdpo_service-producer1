5ac1932a8b521bb827ce96dd0501d61a02609919
Galiandina <108342736+feyerinaGO@users.noreply.github.com>
2023-05-23 00:35:47 +0300

    task 5 elk

--- src/main/java/ru/bit/producer1/controller/Producer1Controller.java
+++ src/main/java/ru/bit/producer1/controller/Producer1Controller.java
@@ -1,5 +1,6 @@
 package ru.bit.producer1.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -7,6 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("convert")
+@Log4j2
 public class Producer1Controller {
     @GetMapping("from/{firstCurrency}/to/{secondCurrency}")
     public Map<String, String> exchange(@PathVariable String firstCurrency, @PathVariable String secondCurrency,
@@ -16,6 +18,7 @@ public class Producer1Controller {
         result.put(secondCurrency, returningValue + "");
         String instanceName = "Producer1";
         result.put("instanceId", instanceName);
+        log.info(instanceName + " " + value + " " + firstCurrency + " = " + returningValue + " " + secondCurrency);
         return result;
     }
 }
